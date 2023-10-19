65b32297eeb7681fc49b8f6113dee82ab7a1e135
George Kazibwe <gkazibwe@gmail.com>
2023-10-19 12:25:45 -0700

    increasing server limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increasing the amount of traffic an Nginx server can handle
+
+# Fix and allow unlimited traffic/requests
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restarting Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
